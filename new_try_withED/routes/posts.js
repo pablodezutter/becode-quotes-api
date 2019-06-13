3856abf1eb3ede6ecada0405bb241086d4ab9a93
pablodezutter <dezutterpablo@gmail.com>
2019-06-13 15:48:28 +0200

    getting all posts

--- new_try_withED/routes/posts.js
+++ new_try_withED/routes/posts.js
@@ -5,9 +5,15 @@ const Post= require('../models/Post'); // double dots!!
 
 
 
-router.get('/', (req,res)=>{
-    res.send('we are on posts');
+router.get('/', async(req,res)=>{
+    //res.send('we are on posts');
+try{
+    const post= await Post.find(); // method in mongoose
+    res.json(posts);
 
+}catch(err){
+    res.json({ message: err });
+}
 });
 
 router.get('/specific', (req,res)=>{
@@ -17,7 +23,7 @@ router.get('/specific', (req,res)=>{
 
 
 
-router.post('/', (req, res) =>{
+router.post('/', async (req, res) =>{
     // console.log(req.body);
     const post= new Post ({
         title: req.body.title,
@@ -26,16 +32,26 @@ router.post('/', (req, res) =>{
     });
 
     //Create promise
+try{
+   const savedPost= await post.save();
+    //      .then(data =>{
+    //     res.json(data);
+    // })
+    // .catch(err => {
+    //     res.json({ message: err });
 
-    post.save()
-        .then(data =>{
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({ message: err });
+
+    // }); 
+
+//Simplefy this:
+    res.json(savedPost);
+}catch(err){
+    res.json({ message: err });
+
+}
 
 
-    }); 
+
 
 });
 
