778a283facb028369018208eb24967ea69e20ec0
pablodezutter <dezutterpablo@gmail.com>
2019-06-13 16:23:55 +0200

    add, find and delete posts

--- new_try_withED/routes/posts.js
+++ new_try_withED/routes/posts.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const Post= require('../models/Post'); // double dots!!
 
 
-
+    //get post
 
 router.get('/', async(req,res)=>{
     //res.send('we are on posts');
 try{
-    const post= await Post.find(); // method in mongoose
+    const posts= await Post.find(); // method in mongoose
     res.json(posts);
 
 }catch(err){
@@ -16,14 +16,11 @@ try{
 }
 });
 
-router.get('/specific', (req,res)=>{
-    res.send('specific post');
-
-});
 
 
+    //submit post
 
-router.post('/', async (req, res) =>{
+router.post('/', async(req, res) =>{
     // console.log(req.body);
     const post= new Post ({
         title: req.body.title,
@@ -50,10 +47,32 @@ try{
 
 }
 
+});
 
 
+//specific post
+
+router.get('/:postId', async(req,res)=>{
+    try{
+    //console.log(req.params.postId);
+   const post= await Post.findById(req.params.postId);
+   res.json(post);
+    }catch(err){
+        res.json({message:err});
+
+    }
 
 });
 
+//delete specific post
+router.delete('/:postId', async(req,res) =>{
+    try{
+    const removedPost= await Post.remove({_id: req.params.postId });
+    res.json(removedPost);
+    }catch(err){
+        res.json({message:err});
+
+    }
+});
 
 module.exports = router;
